50eefcb5aeb45d7d27207f942543d6caf9dc9f43
Ander <ander.uraga@gmail.com>
2016-10-22 20:07:14 +0200

    podemos guardar nombres a través de alert

--- Swift3CoreDataApp/ViewController.swift
+++ Swift3CoreDataApp/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource {
 
+    @IBOutlet var tableView: UITableView!
+    var names = [String]()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        title = "Listado Nombres"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    //Implement the addName IBAction
+    @IBAction func addName(sender: AnyObject) {
+        
+        let alert = UIAlertController(title: "Nuevo Nombre",
+                                      message: "Añade un nombre",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Salvar",
+                                       style: .default,
+                                       handler: { (action:UIAlertAction) -> Void in
+                                        
+                                        let textField = alert.textFields!.first
+                                        self.names.append(textField!.text!)
+                                        self.tableView.reloadData()
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .default) { (action: UIAlertAction) -> Void in }
+        
+        alert.addTextField { (textFiled) in }
+        
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present( alert, animated: true, completion: nil)
+    }
+    
+    //MARK: UITableViewDelegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell!.textLabel!.text = names[indexPath.row]
+        return cell!
+    }
+    
+    
+    
 }
 
